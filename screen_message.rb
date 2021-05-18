0ea008b987d3fa2da6215a5ebebc6fa694fc915a
petitejess <jessica.gozali@outlook.com.au>
2021-05-18 22:36:22 +1000

    Add welcome, instructions, prologue messages

--- screen_message.rb
+++ screen_message.rb
@@ -7,10 +7,11 @@ class ScreenMessage
   def initialize
   end
 
+  # Method for displaying message frame
   def msg_frame(title, msg, height = 10)
     msg_box = TTY::Box.frame({
       enable_color: true, # force to always color output
-      width: TTY::Screen.width - 1,
+      width: 150,
       height: height,
       align: :center,
       padding: 3,
@@ -32,20 +33,23 @@ class ScreenMessage
   
   def display_welcome
     title = " WELCOME "
-    msg = "Hello there... Welcome to Ruby Burger!"
-    msg_frame(title, msg)
+    msg = "Hello there... Welcome to Ruby Burger!\n\n\n~ END ~"
+    height = 13
+    msg_frame(title, msg, height)
   end
 
   def display_instructions
     title = " HOW TO PLAY "
-    msg = "Instructions"
-    msg_frame(title, msg)
+    msg = "Instructions:\n\n1. When shop's Menu is displayed, memorise burger names, ingredients and quantity needed, and ingredients stack order.\n\n2. Customer's request will consists of the name of the burger, and custom preferences, such as 'no sauce' or 'extra 1 patty'.\n\n3. Enter ingredient and quantity needed in order, from bottom of the stack to the top.\n\n4. Keep customers happy and earn money to meet the target (You Win).\n\n5. Be careful not to make the customer angry and let your reputation get to 0 (Game Over).\n\n\n~ END ~"
+    height = 23
+    msg_frame(title, msg, height)
   end
   
   def display_prologue
     title = " PROLOGUE "
-    msg = "Once upon a time ..."
-    msg_frame(title, msg)
+    msg = "Ruby Burger is dedicated in fulfilling the ever growing customers' demand of customised and personalised burgers.\n\nOur job is to build a burger following our shop's Menu recipes, while adjusting into different customers' preferences.\n\n In the next scene, we will memorize our Menu and recipes.\n\nHint: Memorise burger names, ingredients and quantity needed, and ingredients stack order. Build burger from bottom up. Enter ingredients from the bottom of the stack to the top as seen in the recipes.\n\n\n~ END ~"
+    height = 19
+    msg_frame(title, msg, height)
   end
 end
 
