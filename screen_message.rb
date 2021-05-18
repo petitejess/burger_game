2b8c4eccabce8b06f7dc652e2a3b95405f3f3cbb
petitejess <jessica.gozali@outlook.com.au>
2021-05-18 20:14:57 +1000

    Create class and methods to pass test for Feature 1 display

---
+++ screen_message.rb
@@ -0,0 +1,16 @@
+class ScreenMessage
+    def initialize
+    end
+    
+    def display_welcome
+        "Hello"
+    end
+
+    def display_instructions
+        "How to Play"
+    end
+    
+    def display_prologue
+        "Prologue"
+    end
+end
