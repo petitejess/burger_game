eeb867232260ec0e03b0e5ef067fcd24999f00e5
petitejess <jessica.gozali@outlook.com.au>
2021-05-19 13:41:42 +1000

    Add test case for Feature 4 display options

--- spec/ruby_burger_game_spec.rb
+++ spec/ruby_burger_game_spec.rb
@@ -2,40 +2,52 @@ require_relative '../main'
 require_relative '../screen_message'
 require_relative '../recipe'
 
-# Test case for Feature 1
-describe ScreenMessage do
-  # This block runs before each test case defined in 'it' block
+# # Test case for Feature 1
+# describe ScreenMessage do
+#   # This block runs before each test case defined in 'it' block
+#   before(:each) do
+#     @screen_message = ScreenMessage.new
+#   end
+
+#   it "check if display_welcome is defined" do
+#     expect(@screen_message.display_welcome.class).to eq(String)
+#   end
+
+#   it "check if display_instructions is defined" do
+#     expect(@screen_message.display_instructions.class).to eq(String)
+#   end
+
+#   it "check if display_prologue is defined" do
+#     expect(@screen_message.display_prologue.class).to eq(String)
+#   end
+# end
+
+# # Test case for Feature 2
+# describe Recipe do
+#   # This block runs before each test case defined in 'it' block
+#   before(:each) do
+#     @name = "Basic Burger"
+#     @ingredients = [{ "Bun" => 1 }, { "Tomato Sauce" => 1 }, { "Lettuce" => 1 }, { "Grilled Chicken" => 2 }, { "Cheese" => 1 }, { "Bun" => 1 }]
+#     @recipe = Recipe.new(@name, @ingredients)
+#   end
+
+#   it "should display the recipe" do
+#     expect(@recipe.display_recipe.class).to eq(String)
+#   end
+
+#   it "should return the recipe ingredients" do
+#     expect(@recipe.get_ingredients(@name)).to eq(@ingredients)
+#   end
+# end
+
+# Test case for Feature 4
+describe PlayerOption do
   before(:each) do
-    @screen_message = ScreenMessage.new
+    options = ["Bun", "Tomato Sauce", "Lettuce", "Grilled Chicken", "Cheese"]
+    @options = PlayerOption.new(options)
   end
 
-  it "check if display_welcome is defined" do
-    expect(@screen_message.display_welcome.class).to eq(String)
-  end
-
-  it "check if display_instructions is defined" do
-    expect(@screen_message.display_instructions.class).to eq(String)
-  end
-
-  it "check if display_prologue is defined" do
-    expect(@screen_message.display_prologue.class).to eq(String)
-  end
-end
-
-# Test case for Feature 2
-describe Recipe do
-  # This block runs before each test case defined in 'it' block
-  before(:each) do
-    @name = "Basic Burger"
-    @ingredients = [{ "Bun" => 1 }, { "Tomato Sauce" => 1 }, { "Lettuce" => 1 }, { "Grilled Chicken" => 2 }, { "Cheese" => 1 }, { "Bun" => 1 }]
-    @recipe = Recipe.new(@name, @ingredients)
-  end
-
-  it "should display the recipe" do
-    expect(@recipe.display_recipe.class).to eq(String)
-  end
-
-  it "should return the recipe ingredients" do
-    expect(@recipe.get_ingredients(@name)).to eq(@ingredients)
+  it "should display player's options" do
+    expect(@options.display_options).to eq(String)
   end
 end
