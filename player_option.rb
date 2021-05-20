0664d59038b9771c000847209a7fc2e2ff2c45cb
petitejess <jessica.gozali@outlook.com.au>
2021-05-20 16:36:10 +1000

    Create test case and code to pass test for Feature 3 display customer response

--- player_option.rb
+++ player_option.rb
@@ -8,16 +8,6 @@ class PlayerOption
   def initialize
   end
 
-  # def add_selected(item, quantity)
-  #   selected_item = SelectedItem.new(item, quantity)
-  #   @selected_items << selected_item
-  # end
-
-  # def get_quantity(item)
-  #   selected = @selected_items.find { |selected_item| selected_item.item === item}
-  #   selected.quantity
-  # end
-
   def get_options
     # Create instance of TTY Prompt
     prompt = TTY::Prompt.new
